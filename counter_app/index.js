b72e5426bf76300613d4c24850e731d5807ec566
SammyBloom <nwachiemi@gmail.com>
2023-03-07 22:49:39 +0100

    created a variable, an increment function and link Javascript files to HTML files

---
+++ counter_app/index.js
@@ -0,0 +1,13 @@
+// document.getElementById("count").innerText = 5
+
+// Initialize the count as 0
+let count = 0;
+
+// Listen for clicks on the increment button
+function increment() {
+    console.log("The button was clicked")
+}
+
+// Increment the count variable when increment button is clicked
+
+//change the count in the HTML to relect the bew count
