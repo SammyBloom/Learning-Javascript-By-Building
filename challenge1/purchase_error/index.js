26a466dbaa9b08966bd06249f56c1be7cf71e79e
SammyBloom <nwachiemi@gmail.com>
2023-03-21 14:29:24 +0100

    completed the purchase_error challenge and other challenges in challenge1.js

---
+++ challenge1/purchase_error/index.js
@@ -0,0 +1,7 @@
+let purchaseItem =  document.getElementById("error")
+
+let error = "SOmething went wrong...";
+
+function errorMessage(){
+    purchaseItem.textContent = error;
+}
---
+++ counter_app/challenge1.js
@@ -0,0 +1,43 @@
+// Create two variables
+let firstName = "Samuel";
+let lastName = " Nwanwobi";
+
+// Concatenate the two varaibales
+function greeting(){
+    let fullName = firstName + lastName;
+
+    //Log Fullname
+    console.log(fullName);
+}
+
+greeting();
+
+
+let myPoints = 3;
+
+// Create two functions to add 3 points and remove 1 point from myPoints
+function add3Points(){
+    myPoints += 3
+}
+
+function remove1Point(){
+    myPoints -= 1
+}
+
+add3Points();
+add3Points();
+remove1Point();
+remove1Point();
+
+console.log(myPoints);
+
+
+let purchaseItem =  document.getElementById("error")
+
+let error = "SOmething went wrong...";
+
+function errorMessage(){
+    purchaseItem.textContent = error;
+}
+
+
