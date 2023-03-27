eeec855e9dfe7d6f00eae3d157fc6b7b838669cf
SammyBloom <nwachiemi@gmail.com>
2023-03-27 15:31:57 +0100

    created the startGame function and the game can be played with more ellaborate informations like the sum of the cards picked and the value of the 1st and 2nd cards

--- blackjack_app/index.js
+++ blackjack_app/index.js
@@ -1,11 +1,15 @@
 // create two variables, firstCard and secondCard
 // Set their values to a random number between 2 and 11
+let messagePrompt = document.getElementById("message-prompt");
+// let sum = document.getElementById("sum-cards");
+let sum = document.querySelector(".sum-cards");
+let card = document.querySelector("#cards");
+
 let firstCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
-console.log(firstCard);
 let secondCard = Math.floor(Math.random() * (11 - 2 + 1)) + 2;
 
 // Sum up the two cards
-let sum = firstCard + secondCard;
+let sumCard = firstCard + secondCard;
 
 //check if blackJack condition was met
 let hasBlackJack = false;
@@ -16,18 +20,20 @@ let isAlive = true;
 // A varaible called message and assign a string value to it
 let message = "We're logging out";
 
-//conditionals
-if (sum <= 20){
-    message = "Do you want to draw another card? 🙂";
-} else if (sum === 21){
-    message = "Congratulations 🥳";
+function startGame() {
+    sum.textContent = "Sum: " + sumCard;
+    card.textContent = "Cards: " + firstCard + " " + secondCard;
+  //conditionals
+  if (sumCard <= 20) {
+    message = "Do you want to draw another card?";
+  } else if (sumCard === 21) {
+    message = "Congratulations";
     hasBlackJack = true;
-} else {
-    message = "You're out of the game. Try again later 😢";
+  } else {
+    message = "You're out of the game. Try again later";
     isAlive = false;
-}
+  }
 
-// CASH OUT
-console.log(hasBlackJack);
-console.log(isAlive);
-console.log(message);
+  // CASH OUT
+  messagePrompt.textContent = message;
+}
