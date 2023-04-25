0eab2cee856ae8b1f9555391de28146c61d4b05d
SammyBloom <nwachiemi@gmail.com>
2023-04-25 17:12:32 +0100

    started the lead tracker app. Created a textinput field and a button in html. used the addEventListener to trigger a response from clicking the button

---
+++ lead_tracker_extension/index.js
@@ -0,0 +1,8 @@
+const inputSa = document.getElementById("input-sa");
+const inputBtn = document.getElementById("input-btn");
+
+let myLeads = [];
+
+inputBtn.addEventListener("click", function(){
+    console.log("Button clicked from event listener");
+});
---
+++ lead_tracker_extension/practise/index.js
@@ -0,0 +1,5 @@
+let box = document.getElementById("open-box");
+
+box.addEventListener("click", function(){
+    console.log("I want to open the box");
+})
