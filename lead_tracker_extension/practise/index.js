7a05f403ddb8dc47e54a52b0096bdbb47596b4f2
SammyBloom <nwachiemi@gmail.com>
2023-05-02 14:08:06 +0100

    implemented the onclick function in html in a javascript file

--- lead_tracker_extension/practise/index.js
+++ lead_tracker_extension/practise/index.js
@@ -1,5 +1,13 @@
 let box = document.getElementById("open-box");
+let buyBtn =  document.getElementById("container");
 
 box.addEventListener("click", function(){
     console.log("I want to open the box");
 })
+
+buyBtn.innerHTML = "<button onclick='buy()'>BUY!</button>";
+
+function buy() {
+    buyBtn.innerHTML += "<p>Thank you for buying!</p>"
+}
+
