08e55b7c1124232fdf1b36a4313ad4cee6b8986d
SammyBloom <nwachiemi@gmail.com>
2023-05-02 13:07:19 +0100

    implemented the display of lists by looping through the myLeads array

--- lead_tracker_extension/index.js
+++ lead_tracker_extension/index.js
@@ -1,8 +1,14 @@
 const inputSa = document.getElementById("input-sa");
 const inputBtn = document.getElementById("input-btn");
+const ulSa = document.getElementById("ul-sa");
 
-let myLeads = [];
+let myLeads = ["google", "facebook", "amazon", "spotify", "tinder"];
 
 inputBtn.addEventListener("click", function(){
-    console.log("Button clicked from event listener");
+    myLeads.push(inputSa.value);
+    console.log(myLeads);
 });
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulSa.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
